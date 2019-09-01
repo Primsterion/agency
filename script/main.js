424d1313c574ddc7b1887b08c996fb74d7285388
Primsterion <47631269+Primsterion@users.noreply.github.com>
2019-09-01 17:39:21 +0300

    Внесение правок
    Доработка скролла

--- script/main.js
+++ script/main.js
@@ -1,49 +1,48 @@
 "use strict"
 
 //Переменные
-const bars = document.getElementsByClassName('bar')[0]; //Кнопка для открытия меню
-const menu = document.getElementsByClassName('m_menu')[0]; //Блок мобильного меню
-const close = document.getElementsByClassName('close_menu')[0]; //Кнопка закрытия меню
-const homeLink = document.querySelector('.home'); // Ссылка на главную страницу
-const navLinks = document.querySelectorAll('#nav_desktop .nav__item'); // Список ссылок в шапке (десктоп версия)
-const mobileLinks = document.querySelectorAll('.mobile_menu .nav__item'); // Список ссылок в шапке (мобильная версия)
-const scrollBtn = document.querySelector('.scroll-up'); // Кнопка скролла на верх
-const scroll_btn = document.getElementById("scroll_btn");// Кнопка скролла в шапке
-const header = document.querySelector('header');
+const bars = document.querySelector('.bar'), //Кнопка для открытия меню
+ menu = document.querySelector('.m_menu'), //Блок мобильного меню
+ close = document.querySelector('.close_menu'), //Кнопка закрытия меню
+ homeLink = document.querySelector('.home'), // Ссылка на главную страницу
+ navLinks = document.querySelectorAll('#nav_desktop .nav__item'), // Список ссылок в шапке (десктоп версия)
+ mobileLinks = document.querySelectorAll('.mobile_menu .nav__item'), // Список ссылок в шапке (мобильная версия)
+ scrollBtn = document.querySelector('.scroll-up'), // Кнопка скролла на верх
+ scroll_btn = document.querySelector("#scroll_btn");// Кнопка скролла в шапке
 
+//Сброс активных стилей для ссылок
 let resetLinks = () => {
 	navLinks.forEach((link) => {
 		link.classList.remove('active');
 	});
 }
 
-//Отобразить мобильное менб
-bars.addEventListener('click', function () {
+//Отобразить мобильное меню
+bars.addEventListener('click', () => {
 	menu.style.transform = 'translateX(0%)';
 });
 
 //Скрыть мобильное меню
-close.addEventListener('click', function () {
+close.addEventListener('click', () => {
 	menu.style.transform = 'translateX(-100%)';
 });
 
 //Скролл к указаному элементу
 
-scroll_btn.addEventListener('click', function () {
-	const scroll_el = document.getElementsByClassName('content')[0];
-	const scroll = scroll_el.offsetTop;
-	window.scrollTo(0, scroll);
+scroll_btn.addEventListener('click', () => {
+	const scroll_el = document.querySelector('.content');
+	window.scrollTo(0, scroll_el.offsetTop);
 });
 
 //Обработка нажатий на ссылки мобильного меню
 
 mobileLinks.forEach((link) => {
 	link.addEventListener('click', () => {
-		menu.style.transform = 'translateX(-100%)'; //Скрытие мобильного меню
+		menu.style.transform = 'translateX(-100%)'; //Скрытие мобильного меню после нажатия на ссылку
 	});
 })
 
-//Обработка скролла страницы. Если в активной области шапка сайта, элемент меню Home становиться активным
+//Обработка скролла страницы. Если в активной области шапка сайта, то элемент меню Home становиться активным
 window.addEventListener('scroll', () => {
 	if (this.scrollY < 100) {
 		resetLinks();
@@ -56,26 +55,7 @@ window.addEventListener('scroll', () => {
 	}
 });
 
+//Скролл к началу
 scrollBtn.addEventListener('click', () => {
 	window.scrollTo(0, 0);
 });
-
-//Инициализация карты
-var myMap;
-ymaps.ready(init);
-
-function init() {
-	myMap = new ymaps.Map("map", {
-		center: [55.76, 37.64],
-		zoom: 10
-	});
-}
-
-
-navLinks.forEach((item) => {
-	item.addEventListener('click', function () {
-		resetLinks();
-		this.classList.add('active');
-	});
-});
-
